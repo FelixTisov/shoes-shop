433805fefcd26045fd9d8d694539cde367cd4630
FelixTisov <fedya002.ks@gmail.com>
2022-08-10 14:32:52 +0300

    добавлены промо товары

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -91,6 +91,7 @@ function Main() {
                     <div className='third-box'>
                         <div className='brand-name-cont'>
                             <span>NIKE</span>
+                            <hr/>
                         </div>
                         <div className='brand-title-cont'>
                             <p>NEW<br/>COLLECTION</p>
@@ -103,12 +104,64 @@ function Main() {
                     </div>
 
                     <div className='third-box'>
-
+                        
+                        <span>AIR MAX</span>
+                        <div className='details-button'>
+                            <div className='details-button-span-cont'>
+                                <span>Details</span>
+                            </div>
+                            <div className='details-button-arrow-cont'>
+                                <div className='arrow'>
+                                    <div id='arrow-right'/>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                    
                 </div>
+
+                {/* Промо товары */}
                 <div className='bottom-cont'>
 
+                        <div class="rounded-cont">
+                            <div class="rouned-item rouned-item-active">
+                                <div class="photo-cont" >
+                                    <div class="photo" id="ph1"></div>
+                                </div>
+                                <div class="price"><p>800$</p></div>
+                            </div>
+                        </div>
+                        <div class="rounded-cont">
+                            <div class="rouned-item">
+                                <div class="photo-cont" >
+                                    <div class="photo" id="ph2"></div>
+                                </div>
+                                <div class="price"><p>750$</p></div>
+                            </div>
+                        </div>
+                        <div class="rounded-cont">
+                            <div class="rouned-item">
+                                <div class="photo-cont" >
+                                    <div class="photo" id="ph3"></div>
+                                </div>
+                                <div class="price"><p>820$</p></div>
+                            </div>
+                        </div>
+                        <div class="rounded-cont">
+                            <div class="rouned-item">
+                                <div class="photo-cont" >
+                                    <div class="photo" id="ph4"></div>
+                                </div>
+                                <div class="price"><p>500$</p></div>
+                            </div>
+                        </div>
+
                 </div>
+            </div>
+
+            {/* 3Д карусель */}
+            <div className='object-cont'>
+
             </div>
 
             {/* Основная часть */}
