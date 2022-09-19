cbb00bfcc222522b0ccb614361ef6fa0f51cde07
FelixTisov <fedya002.ks@gmail.com>
2022-09-20 01:12:50 +0300

    3д карусель отдельным компонентом

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,24 +3,10 @@ import {FaChevronRight} from 'react-icons/fa'
 import Header from '../components/header/header'
 import Footer from '../components/footer/footer'
 import Slider from '../components/slider/slider'
+import Carousel from '../components/3d_carousel/carousel'
 import '../styles/animated.css'
 import '../styles/common.css'
 import '../styles/main.css'
-
-import { Canvas} from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import * as THREE from 'three'
-
-/* Импорт 3-д моделей */ 
-import First from '../models/first'
-import Second from '../models/second'
-import Third from '../models/third'
-
-/* Настройки камеры */
-let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000 );
-camera.position.x = 7  //вокруг центра по плоскости
-camera.position.y = 0 //по вертикали
-camera.position.z = 0
 
 /* Сдвиг бг при прокрутке */ 
 window.onscroll = function () {
@@ -55,100 +41,6 @@ function Main() {
 
     const promoImages = [image1, image2, image3, image4]
     const [shopImage, setShopImage] = useState(promoImages[0])
-
-    const [models] = useState([Third, First, Second])
-    const [model, setModel] = useState(models[1])
-
-    // Смена 3д-модели
-    const handleDotClick = async (index, id) => {
-
-        setModel(models[index])
-
-        let prevDot = document.querySelector('.dot-active')
-        prevDot.classList.remove("dot-active")
-
-        let currentDot = document.getElementById(id);
-        currentDot.classList.add("dot-active")
-
-        switch (id) {
-            case 'dot-top':
-                animateDotTop(index)
-                models.unshift(...models.splice(2,1))
-                break;
-            case 'dot-bottom':
-                animateDotBottom()
-                models.push(...models.splice(0,1))
-                break;
-            default:
-                break;
-        }
-        
-        setTimeout(() => {
-            currentDot.classList.remove("dot-active")
-            prevDot.classList.add("dot-active")
-        }, 730)  
-        
-    }
-
-    // Прокрутка дуги выбора вниз
-    const animateDotTop = () => {
-
-        let oppositDot = document.getElementById('box-dot-bottom')
-        oppositDot.style.visibility='hidden'
-
-        let dotTop = document.getElementById('box-dot-top')
-        dotTop.animate([
-            {transform: 'rotate(0deg) translateX(0px) translateY(0px)'},
-            {transform: 'rotate(43deg) translateX(320px) translateY(45px)'}
-        ], {
-            duration: 700,
-            iterations: 1,
-        })
-
-        let dotMiddle = document.getElementById('box-dot-middle')
-        dotMiddle.animate([
-            {transform: 'rotate(0deg) translateX(0px) translateY(0px)'},
-            {transform: 'rotate(41deg) translateX(7px) translateY(319px)'}
-        ], {
-            duration: 700,
-            iterations: 1,
-        })
-
-        setTimeout(() => {
-            oppositDot.style.visibility='visible'
-        }, 700)
-        
-    }
-
-    // Прокрутка дуги выбора вверх
-    const animateDotBottom = () => {
-
-        let oppositDot = document.getElementById('box-dot-top')
-        oppositDot.style.visibility='hidden'
-
-        let dotMiddle = document.getElementById('box-dot-middle')
-        dotMiddle.animate([
-            {transform: 'rotate(0deg) translateX(0px) translateY(0px)'},
-            {transform: 'rotate(-38deg) translateX(4px) translateY(-323px)'}
-        ], {
-            duration: 700,
-            iterations: 1,
-        })
-
-        let dotTop = document.getElementById('box-dot-bottom')
-        dotTop.animate([
-            {transform: 'rotate(0deg) translateX(0px) translateY(0px)'},
-            {transform: 'rotate(-43deg) translateX(320px) translateY(-45px)'}
-        ], {
-            duration: 700,
-            iterations: 1,
-        })
-
-        setTimeout(() => {
-            oppositDot.style.visibility='visible'
-        }, 700)
-
-    }
 
     // Анимация кнопки Order Now
     const arrowOffset = () => {
@@ -360,56 +252,8 @@ function Main() {
                     </div>
                 </div>
 
-                <div className='object-cont-right'>
-                    <div className='canvas-cont'>
-                        <Canvas className='canvas' camera={camera}>
-                            {/* Настройки сцены */}
-                            <OrbitControls 
-                                minPolarAngle={Math.PI/2.5} 
-                                maxPolarAngle={Math.PI/2.5} 
-                                enabled={true} 
-                                position={[7, 7, 7]}
-                                ro
-                                maxDistance={6} 
-                                minDistance={6} 
-                                autoRotate={true}
-                                autoRotateSpeed={5} 
-                            />
-                            <ambientLight color={'#fff'} intensity={0.6}/>
-                           
-                            {/* <mesh>
-                                <boxGeometry attach="geometry" args={[3, 2, 1]} />
-                                <meshPhongMaterial attach="material" color="hotpink" />
-                            </mesh> */}
-                            
-                            {/* Текущая 3д модель */}
-                            { model }
-
-                        </Canvas>
-                    </div>
-                    
-                    <div className='radius'>
-
-                        <div className='box-dot' id='box-dot-top'>
-                            <div className='dot' id='dot-top'  onClick={() => handleDotClick(0, 'dot-top')}/>
-                        </div>
-
-                        <div className='box-dot' id='box-dot-middle'>
-                            <div className='dot dot-active' id='dot-middle' />
-                        </div>
-
-                        <div className='box-dot' id='box-dot-bottom'>
-                            <div className='dot' id='dot-bottom' onClick={() => handleDotClick(2, 'dot-bottom')} />
-                        </div>
-
-                    </div>
-
-                    <div className='object-cont-tools'>
-                        <span className='object-cont-button'>Compare</span>
-                        <span className='object-cont-button'>Learn more</span>
-                        <span className='object-cont-button'>To cart</span>
-                    </div>
-                </div>
+                <Carousel/>
+                
             </div>
             
             {/* Ленты с надписями */}
